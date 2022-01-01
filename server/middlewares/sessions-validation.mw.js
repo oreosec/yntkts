52fcb553bf2045d55b06c3c1a0316001f6c73b29
fwwz-id <fwwz.id@gmail.com>
2022-01-01 12:48:37 +0700

    Fixing middleware nyangkut

--- server/middlewares/sessions-validation.mw.js
+++ server/middlewares/sessions-validation.mw.js
@@ -1,6 +1,7 @@
 const sessionsValidate = (req, res, next) => {
     // if (req.session.token) next();
     // else res.status(400).json({ message: "Can't resolve sessions" });
+    next();
 };
 
 module.exports = sessionsValidate;
